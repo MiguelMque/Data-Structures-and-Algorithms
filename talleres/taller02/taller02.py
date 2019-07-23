9a20585a50447a4b916103fc74df535c99a2473a
miguelmque <41609302+MiguelMque@users.noreply.github.com>
2019-07-23 15:49:10 -0500

    Add files via upload
    solucion taller 02

---
+++ talleres/taller02/taller02.py
@@ -0,0 +1,71 @@
+
+"""
+@author Miguel Angel Correa Manrique - Pablo Buitrago Jaramillo
+
+Taller02 Estructura datos y algorítmos
+"""
+#PUNTO 1
+def gcd(a, b):
+  if b == 0:
+    return a
+  return gcd(b, a%b) 
+
+
+
+#PUNTO 2.1
+def check_w(w, l):
+
+    b = False
+    
+    for x in l:
+    
+      remaining = [y for y in l if y!=x]
+    
+      other = other or check_w(remaining)
+    
+    return other
+#PUNTO 2.2
+def check_w2(n, l, w):
+    if n == len(l):
+        return (w == 0)
+    if check_w2(n+1, l, w) or check_w2(n+1, l, w - l[n]):
+        return True
+    return False
+
+
+#PUNTO 3
+def combinations(sequence):
+    
+    if len(sequence) == 1:
+        return [sequence,]
+       
+    l = []
+   
+    for x in sequence:
+        remaining = [w for w in sequence if w!=x]
+        other = combinations(remaining)
+       
+        for z in other:
+            l.append([x] + z)
+           
+    return l
+
+
+def print_possibilities(l):
+
+    n = combinations(l)
+        
+    for x in l:
+        remaining = [w for w in l if w!=x]
+        
+        n = n + combinations(remaining)
+        
+        n = n + [x]
+        
+    
+    n = n + []
+    
+    return n
+        
+
+    
