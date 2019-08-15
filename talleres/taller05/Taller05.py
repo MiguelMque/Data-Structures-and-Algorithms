17da75aa5f26b08ae975ae8064abcb6cbcf5343b
pablo4buitrago <52968548+pablo4buitrago@users.noreply.github.com>
2019-08-14 21:39:17 -0500

    Update Taller05.py

--- talleres/taller05/Taller05.py
+++ talleres/taller05/Taller05.py
@@ -1,9 +1,13 @@
+"""
+@author Miguel Angel Correa Manrique - Pablo Buitrago Jaramillo
+Taller05 Estructura datos y algorítmos
+"""
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-
+#EXCERCISE 1
 def insertionSort(arr):
 
     for i in range(0, len(arr)):
@@ -15,55 +19,72 @@ def insertionSort(arr):
                 arr[j] = arr[temp_pos]
                 arr[temp_pos] = temp
                 temp_pos = j
-
-
-
-
-
     return arr
+    #O(n^2)
 
+#EXCERCISE 2
+def ar_sum(arr):
+    sum = 0
+    for i in range(len(arr)):
+        sum += arr[i]
+    
+    return sum
+    #O(n)
 
 def milli():
 
     return time.time() * 1000000
 
 
-for i in range(1,20):
+def plots():
 
     x1 = []
     y1 = []
 
+    x2 = []
+    y2 = []
+
+    for i in range(1,20):
+        
+        l = []
+
+        for w in range(0,i):
+            l.append(i)
+
+        start = milli()
+        insertionSort(l)
+        end = milli() - start
+
+        x1.append(i)
+        y1.append(end)
+
+        start2 = milli()
+        ar_sum(l)
+        end2 = milli() - start2
+
+        x2.append(i)
+        y2.append(end2)
+    
+    print(y1)
+    print()
+    print(y2)
+
     l = []
-
-    for w in range(0,i):
-        l.append(i)
-
-    start = milli()
-    insertionSort(l)
-    end = milli() - start
-
-    x1.append(i)
-    y1.append(end)
-
-    l = []
-
-    for w in range(0,i):
-        l.append(i)
-
-    start = milli()
-    insertionSort(l)
-    end = milli() - start
-
-    x1.append(i)
-    y1.append(end)
-
-    l = []
-
-    print(end)
 
     plt.figure()
     plt.subplot(2,2,1)
     plt.plot(x1, y1, 'ro')
-    plt.axis([0, 20, 0, 55])
+    plt.axis([0, 20, 0, 28])
+    plt.title('insertionSort')
 
+    plt.subplot(2,2,2)
+    plt.plot(x2, y2, 'ro')
+    plt.axis([0, 20, 0, 5])
+    plt.title('arr_sum')
+    
+    plt.show()
+    
+    
+
+plots()
 
