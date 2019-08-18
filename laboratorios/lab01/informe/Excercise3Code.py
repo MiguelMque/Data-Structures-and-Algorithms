7e849ddc0c739dbde0cfe146f56d109a9277609d
pablo4buitrago <52968548+pablo4buitrago@users.noreply.github.com>
2019-08-18 17:55:55 -0500

    Create Excercise3Code.py
    Cree el archivo y pegué el código correspondiente al punto 3.2

---
+++ laboratorios/lab01/informe/Excercise3Code.py
@@ -0,0 +1,56 @@
+"""
+@author Miguel Angel Correa Manrique - Pablo Buitrago Jaramillo
+Lab 01 Estructura datos y algorítmos
+"""
+
+#Excercise 3.2
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+def milli():
+
+    return time.time() * 1000000
+
+def rect(n):
+    '''
+    Return the number of ways how one can organize 1x2 rectangles in a 2xn rectangle. 
+    '''
+    if n == 1 or n == 2:
+        return n
+
+    arr = [0]
+
+    arr.append(1)
+    arr.append(2)
+
+    for i in range(3, n+1):
+        arr.append(arr[i-1] + arr[i-2])
+    
+    return arr[n]
+
+def plots():
+
+    x1 = []
+    y1 = []
+
+    for i in range(1,20):
+
+        start = milli()
+        rect(i)
+        end = milli() - start
+
+        x1.append(i)
+        y1.append(end)
+
+    
+    print(y1)
+
+    plt.plot(x1, y1, 'ro')
+    plt.axis([0, 20, 0, 25])
+    plt.title('rect')
+
+    
+    plt.show()
+
+plots()
