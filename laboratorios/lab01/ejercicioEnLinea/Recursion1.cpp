fbc47080798260bd965a0fb66c78111935a49ba4
miguelmque <macorream@eafit.edu.co>
2019-08-18 17:26:08 -0500

    exercises changed to work with vectors, pointers are scary but interesting

--- laboratorios/lab01/ejercicioEnLinea/Recursion1.cpp
+++ laboratorios/lab01/ejercicioEnLinea/Recursion1.cpp
@@ -24,8 +24,8 @@ string allStar(string str){
 }
 
 
-bool array220(int *nums, int index){
-	if(index == sizeof(nums)-1 || sizeof(nums) == 0) return false;
+bool array220(vector<int> nums, int index){
+	if(index == nums.size()-1 || nums.size() == 0) return false;
 	
 	if(nums[index]*10 == nums[index+1]) return true;
 
@@ -65,11 +65,11 @@ int main(){
 	cout << "Input: hello" << endl;
 	cout << "Output: " << allStar("hello") << endl << endl;
 
-	int nums[] = {1, 2, 3, 4, 40};
+	vector <int> vector{1, 2, 3, 4, 40};
 	
 	cout << "''Array220''" << endl;
 	cout << "Input: {1, 2, 3, 4, 40}" << endl;
-	cout << "Output: " <<  array220(nums, 0) << endl << endl;
+	cout << "Output: " <<  array220(vector, 0) << endl << endl;
 	
 	cout << "''CountHi2''" << endl;
 	cout << "Input: hixhhi" << endl;
