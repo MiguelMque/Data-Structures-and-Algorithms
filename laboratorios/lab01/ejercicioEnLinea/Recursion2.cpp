cf8c66da60f2b5f1e64cede551facd1863812bb1
miguelmque <macorream@eafit.edu.co>
2019-08-18 17:28:59 -0500

    added groupSum5 with vectors

--- laboratorios/lab01/ejercicioEnLinea/Recursion2.cpp
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include <initializer_list>
 #include <iostream>
-
+#include <array>
 using namespace std;
 
-bool groupSum6(int start, int *nums, int target){
-	if(sizeof(nums)-1 < start){
+bool groupSum6(size_t start, vector<int> nums, int target){
+	if(nums.size()-1 < start){
 		if(target == 0) return true;
 		else return false;
 
@@ -17,16 +17,40 @@ bool groupSum6(int start, int *nums, int target){
 
 }
 
+
+bool groupSum5(size_t start, vector<int> nums, int target){
+
+	if(nums.size()-1 < start) return target == 0;
+
+
+	if(nums[start]%5 == 0 && nums.size()-1 >=  start+1 && nums[start+1] == 1) return groupSum5(start+2, nums, target-nums[start]);
+
+	else if (nums[start]%5 == 0) return groupSum5(start+1, nums, target-nums[start]);
+
+	return groupSum5(start+1, nums, target-nums[start]) || groupSum5(start+1, nums, target);
+
+
+
+
+
+
+}
+
 int main(){
 
 	cout << "''GroupSum6''" << endl;
 	cout << "Input: {5, 6, 2}, 9" << endl;
 
-	int nums[] = {5,6,2};
+	vector <int> vect {5,6,2};
 		
-	cout << "Output: " << groupSum6(0, nums, 9) << endl;
+	cout << "Output: " << groupSum6(0, vect, 9) << endl << endl;
 
+	cout << "''GroupSum5''" << endl;
+	cout << "Input: {2, 5, 10, 4}, 15" << endl;
 	
+	vector<int> vect2 {2, 5, 10, 4};
+	
+	cout << "Output: " << groupSum5(0, vect2, 15) << endl << endl;	
 
 
 return 0;
