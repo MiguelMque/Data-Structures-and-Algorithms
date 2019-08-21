284bffd858f71f0e946c0b5513c12b5aab1d0464
miguelmque <macorream@eafit.edu.co>
2019-08-21 03:09:36 -0500

    optina #2 solved

--- talleres/taller06/Taller06.cpp
+++ talleres/taller06/Taller06.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
+// #1
 class ArrayList{	
 	public:
 		int DEFAULT_SIZE = 4;
@@ -82,11 +85,52 @@ class ArrayList{
 
 			cout << "[ ";
 
-			for(int i = 0; i < size; i++){
-				cout << arr[i] << " ";
-			}
+			for(int i = 0; i < size; i++) cout << arr[i] << " ";
+			
 
 			cout << "]" << endl;	
+
+		}
+
+
+
+		//Optional #2 addReverse Algorithm
+		void addReverse(int x){
+			size += 1;
+
+
+			if(size < DEFAULT_SIZE) arr[DEFAULT_SIZE-size] = x;
+
+			else {
+
+				DEFAULT_SIZE = DEFAULT_SIZE*2;
+
+				int *arr2;
+
+				arr2 = new int[DEFAULT_SIZE];
+			
+				arr2[DEFAULT_SIZE-size] = x;
+
+				for(int i = 1; i < DEFAULT_SIZE/2; i++) arr2[DEFAULT_SIZE-size+i] = arr[i];
+
+				arr = arr2;
+
+			}
+
+			
+		}
+
+		
+
+
+		void printReversed(){
+
+			cout << "[ ";
+
+                        for(int i = DEFAULT_SIZE-size; i < DEFAULT_SIZE ; i++)  cout << arr[i] << " ";
+                        
+
+                        cout << "]" << endl;
 
 		}
 
@@ -94,22 +138,41 @@ class ArrayList{
 
 
 
+//Optional #2 Reader
+void readArray(ArrayList myArray){
+	int x;
+	while(true){
+
+
+		cout << "Put integer value: ";
+
+		cin >> x;
+	
+
+		if(x==-1) break;
+
+		myArray.addReverse(x);
+	}
+
+	myArray.printReversed();
+
+
+
+
+}
+
 
 int main(){
 
-
 	ArrayList myArray;
 	myArray.Init();
-	
-	myArray.add(2);	
-	myArray.addInIndex(0, 0);
-	myArray.addInIndex(1, 1);
-	myArray.del(2);
-	myArray.add(2);
-	myArray.add(3);
-	myArray.add(4);
 
-	myArray.print();
+
+
+	readArray(myArray);
+
+
+
 
 
 
