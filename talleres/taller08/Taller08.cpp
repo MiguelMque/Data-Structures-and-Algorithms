8d4f837a4c692eb050728f233923a555b4830dba
miguelmque <macorream@eafit.edu.co>
2019-08-25 15:17:07 -0500

    Taller08 solved on c++, but without optional points

--- talleres/taller08/Taller08.cpp
+++ talleres/taller08/Taller08.cpp
@@ -15,30 +15,6 @@ int polishNotationCalculator(string str){
 	bool wasN;
 	string num = "";
 
-	/*for(int i = 0; i < str.size(); i++){
-
-		if(str.at(i) == ' ' && wasN){
-			stringstream g(num);
-			int value;
-			g >> value;
-			n.push(value);
-			num = "";
-			wasN = false;
-		} else if (isdigit(str.at(i))) {
-		       	
-			wasN = true;
-			num += str.at(i);
-                
-		} else if (str.at(i)!=' '){
-
-
-			op.push(str.at(i));
-
-		}
-	
-	}*/
-
-
 	for(int i = 0; i < str.size(); i++){
 
 
@@ -101,6 +77,124 @@ int polishNotationCalculator(string str){
 
 
 
+class Fridge{
+
+	int code;
+	string descrip;
+
+	public:
+		void setCode(int c){
+		       	code = c; 
+		}
+		int getCode(){
+		       	return code; 
+		}
+
+		void setDescrip(string d){
+			descrip = d;
+		}
+
+		string getDescrip(){
+
+			return descrip;
+		}
+
+};
+
+
+
+
+class Store{
+
+	queue <Fridge> fridgeList;
+
+	string name;
+
+	int soli;
+
+	public:
+		void setName(string n){
+			name = n;
+		}
+
+		string getName() {
+			return name;
+		}
+
+		void addFridge(Fridge f){
+			fridgeList.push(f);
+			
+		}
+
+		queue<Fridge> getFridgeList(){
+
+			return fridgeList;
+		}
+
+		void popFridge(){
+			fridgeList.pop();
+
+		}
+
+		void setSoli(int s){
+			soli = s;
+		}
+
+		int getSoli(){
+
+			return soli;
+		}
+
+};
+
+void printListStores(vector <Store> solicitudList){
+
+        for(int i = 0; i < solicitudList.size(); i++){
+
+        cout << "For store " << solicitudList[i].getName() << endl;
+
+        if(solicitudList[i].getSoli() == solicitudList[i].getFridgeList().size()) cout << "Supplies satisfied: " << solicitudList[i].getFridgeList().size() << " of " << solicitudList[i].getFridgeList().size() << endl;
+
+        else cout << "Warehouse out of fridges, we could get just: " << solicitudList[i].getFridgeList().size() << " of " << solicitudList[i].getSoli() << endl;
+
+        }
+
+}
+
+
+void point2(vector <Store> solicitudList, Store warehouse){
+	Store temp;
+	Fridge f;
+
+	queue<Fridge> wareFridges;
+
+	warehouse.setName("Almacen");
+
+	int size = warehouse.getFridgeList().size();
+	
+	int size2 = solicitudList.size();
+
+	for(int i = 0; i < size2 && size!= 0; i++){
+
+		temp = solicitudList[i];
+
+		for(int j = 0; j < temp.getSoli() && size!= 0; j++){
+			f = warehouse.getFridgeList().front();
+
+			solicitudList[i].addFridge(f);
+			
+			warehouse.popFridge();
+
+			size--;
+		}
+
+	}
+	printListStores(solicitudList);
+
+
+}
+
+
 
 
 
@@ -108,9 +202,41 @@ int polishNotationCalculator(string str){
 int main(){
 
 
+	vector <Store> solicitudList;
+	//cout <<	polishNotationCalculator("6 5 - 4 +");	
 
-	cout <<	polishNotationCalculator("6 5 - 4 +");	
 
+	Store warehouse;
+
+	Fridge f;
+	for(int i = 0; i <= 10; i++){
+		
+		f.setCode(i);
+
+		f.setDescrip((to_string(i)));
+
+		warehouse.addFridge(f);
+
+	}
+
+
+	Store eafit;
+
+	eafit.setName("EAFIT");
+
+	eafit.setSoli(10);
+
+	solicitudList.push_back(eafit);
+
+	Store eafit2;
+
+	eafit2.setName("EAFIT2");
+
+	eafit2.setSoli(2);
+
+	solicitudList.push_back(eafit2);
+
+	point2(solicitudList, warehouse);
 
 	return 0;
 
