cf3f83a5f8d905d475ff7bccec915c7866692bae
pablo4buitrago <52968548+pablo4buitrago@users.noreply.github.com>
2019-08-31 22:36:53 -0500

    Update Array3.java
    Added missing exercises

--- laboratorios/lab02/ejercicioEnLinea/Array3.java
+++ laboratorios/lab02/ejercicioEnLinea/Array3.java
@@ -109,5 +109,33 @@ public class Array3{
 		return arr;
 
 	}
+	
+	public boolean canBalance(int[] nums) {
+  		int sum1 = 0;
+  		int sum2 = 0;
+  		for(int i=0; i<nums.length; i++){
+   			sum1 += nums[i];
+    			for(int j=i+1; j<nums.length; j++){
+      				sum2 += nums[j];
+    			}
+    			if(sum1 == sum2) return true;
+    			sum2 = 0;
+  		}
+  		return false;
+	}
+	
+	public int maxSpan(int[] nums) {
+  		int span = 0;
+  		int aux = 0;
+  		for(int i=0; i<nums.length; i++){
+    			for(int j=0; j<nums.length; j++){
+      				if(nums[j] == nums[i]) aux = (j-i)+1;
+    			}
+    			span = Math.max(span, aux);
+  		}
+  		return span;
+	}
+
+
 
 }
