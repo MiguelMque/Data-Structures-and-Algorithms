65d6cfd77b05a5577937a523575a44d260bfd141
miguelmque <macorream@eafit.edu.co>
2019-09-08 17:00:43 -0500

    moveOnto successful

--- laboratorios/lab03/ejercicioEnLinea/point2_2.cpp
+++ laboratorios/lab03/ejercicioEnLinea/point2_2.cpp
@@ -4,68 +4,23 @@
 #include <vector>
 using namespace std;
 
-
-
-class Robot{
-
-	public:
-		list<stack<int>> myList;
-
-		Robot(int n){
-
-			list<stack<int>> myList;
-
-        		for(int i = 0; i < n; i++){
-               			stack<int> st;
-              			st.push(i);
-                		myList.push_back(st);
-
-		        }
-
-		}
-
-
-		void moveOnTo(int a, int b){
-			
-
-			
-
-
-		}
-
-
-		void moveOver(int a, int b){
-
-
-		}
-
-
-		void pileOnTo(int a, int b){
-
-
-		}
-
-		void pileOver(int a, int b){
-
-
-		}
-
-};
-
-
-
-
 void printStackList(list<stack<int>> myList){
 
 
 	list<stack<int>>::iterator it;
 
 	for(it=myList.begin(); it!=myList.end(); ++it){
+		int q = -1;
 
 		while(!(*it).empty()){
-		       	cout << (*it).top() << endl;
+		       	cout << (*it).top();
+			
+			q = (*it).top();
+
 			(*it).pop();
 		}
+
+		 if(!(q==-1)) cout << endl;
 
 	}
 	
@@ -74,15 +29,52 @@ void printStackList(list<stack<int>> myList){
 }
 
 
+//goingTo variable indicates if its a or b.
+void cleanAbove(list<stack<int>> *myList, int *pos, int a){
+	
+	list<stack<int>>::iterator it;
+	list<stack<int>>::iterator it2;
+
+	it2 = (*myList).begin();
+	it = (*myList).begin();
+	
+	//It goes until the stack where the value is
+	for(int i = 0; i < pos[a]; i++)	it++;
+
+	while((*it).top()!=a){
+
+		int x = (*it).top();
+
+		(*it).pop();
+
+		pos[x] = x;
+
+		for(int j = 0; j < x; j++) it2++;
+
+		(*it2).push(x);
+		
+		it2 = ((*myList).begin());
+
+	}
 
 
 
-void moveOnto(int a, int b){
 
+}
 
+void moveOnto(list<stack<int>> *myList, int *pos, int a, int b){
 
+	list<stack<int>>::iterator it = (*myList).begin();
+	
+	for(int i = 0; i < pos[a]; i++) it++;
 
+	(*it).pop();
 
+	it = (*myList).begin();
+
+	for(int i = 0; i < pos[b]; i++) it++;
+
+	(*it).push(a);
 
 }
 
@@ -90,7 +82,53 @@ void moveOnto(int a, int b){
 
 int main(){
 
+	//Input variables
+	int n;
+	int a;
+	int b;
+	string command;
+	string option;
 
+	cin >> n;
+
+	int pos[n];
+		
+	list<stack<int>> myList;
+
+	//Creates the list of stacks and also stores the position of the blocks
+		for(int i = 0; i < n; i++){
+               		stack<int> st;
+              		st.push(i);
+			pos[i] = i;
+                	myList.push_back(st);
+
+		}
+
+
+
+	while(cin >> command){
+
+
+		if(command == "quit") break;
+
+		cin >> a >> option >> b;
+
+
+		cout << "A: " << a << "B: " << b << "OPTION: " << option
+			<< "COMMAND: " << command << endl;	
+
+		if(command == "move" && option == "onto"){
+
+
+			cleanAbove(&myList, pos, a);
+			cleanAbove(&myList, pos, b);			
+			moveOnto(&myList, pos, a, b);	
+
+		}
+
+
+	}
 
 	
+	printStackList(myList);
 }
