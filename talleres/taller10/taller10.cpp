75307bdb67a3442212e07e92fe82329ad7995c13
miguelmque <41609302+MiguelMque@users.noreply.github.com>
2019-09-24 11:32:56 -0500

    añadiendo taller10

---
+++ talleres/taller10/taller10.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    Node *left;
+    Node *right;
+    int data;
+    
+};
+
+class BinaryTree{
+    Node *root;
+    
+    public:
+    
+        BinaryTree(){
+            root = NULL;
+        }
+    
+    bool search(int);
+    void add(int);
+    
+    
+    
+};
+
+
+
+void BinaryTree::add(int x){
+    
+    Node *temp = new Node;
+    
+    temp->data = x;
+    temp->right = NULL;
+    temp->left= NULL;
+    
+    if(root == NULL){
+        root = temp;
+        root->left = NULL;
+        root->right = NULL;
+        
+        return;
+    }
+    
+    Node *s = root;
+    Node *temp2 = new Node;
+    
+    while(s->data != x){
+        
+        if (s->data > x && s->left == NULL){
+          s->left = temp;
+          return;
+        } else if (s->data > x && s->left != NULL) s = s->left;
+        
+        else if(s->data < x && s->right == NULL){
+          s->right = temp;
+          return; 
+        } else if (s->data < x && s->right != NULL) s = s->right;
+        
+        
+    }
+
+}
+
+bool BinaryTree::search(int x){
+    
+    Node *s = root;
+    
+    while(s != NULL){
+        
+        if(s->data == x) return true;
+        
+        
+        if(s->data > x) s = s->left;
+        
+        else s = s->right;
+        
+    }
+    
+    
+}
+
+
+int main() {
+    
+    BinaryTree tree;
+    
+    tree.add(2);
+    tree.add(3);
+    tree.add(1);
+    tree.add(4);
+    
+    cout << tree.search(4);
+    
+}
