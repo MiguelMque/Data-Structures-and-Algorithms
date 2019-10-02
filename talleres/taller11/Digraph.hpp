b6ec84b2c4fffd2ba7d94cd3baba1182848215c2
miguelmque <miguelmque@gmail.com>
2019-10-02 06:04:36 -0500

    taller 11 solved

---
+++ talleres/taller11/Digraph.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+class Digraph{
+
+
+    protected:
+            
+    int size;
+
+    Digraph(int s){
+        size = s;
+    }
+
+        struct Point{
+            public:
+                int x;
+                int y;
+
+         };
+    
+    
+
+
+};
